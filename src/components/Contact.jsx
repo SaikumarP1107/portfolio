e0250c298ea902031f33ecca6abdbee73b962804
SaikumarP1107 <126238383+SaikumarP1107@users.noreply.github.com>
2025-03-05 19:35:32 +0100

    Add Vite configuration and implement About and Contact components

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div id = 'contact' style={{width: '100%', height: '100vh'}}>
+      Contact page
+    </div>
+  )
+}
+
+export default Contact
